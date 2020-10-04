6bab027b434070b0ba7a5327400256f75052a7ab
rcagantas <roel.cagantas@gmail.com>
2020-10-04 16:46:24 +0800

    - use sign in with apple button

--- lib/widgets/auth/login_page.dart
+++ lib/widgets/auth/login_page.dart
@@ -3,12 +3,17 @@ import 'package:flutter/widgets.dart';
 import 'package:inventorio2/providers/user_state.dart';
 import 'package:package_info/package_info.dart';
 import 'package:provider/provider.dart';
+import 'package:sign_in_with_apple/sign_in_with_apple.dart';
 
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Consumer<UserState>(
       builder: (context, UserState userState, _) {
+
+        var mediaQuery = MediaQuery.of(context);
+        bool darkMode = mediaQuery?.platformBrightness == Brightness.dark;
+
         return Scaffold(
           body: Center(
             child: IntrinsicWidth(
@@ -28,18 +33,31 @@ class LoginPage extends StatelessWidget {
                     },
                   ),
                   Container(height: 50.0,),
-                  OutlineButton(
+                  FlatButton(
+                    color: darkMode? Colors.white : Colors.blue,
+                    textColor: darkMode? Colors.black: Theme.of(context).canvasColor,
                     key: ObjectKey('google_sign_in'),
                     child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      mainAxisAlignment: MainAxisAlignment.center,
+                      //mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: <Widget>[
-                        Image.asset('resources/icons/google_logo.png', height: 30.0,),
-                        Text('Sign in with Google')
+                        Container(
+                          decoration: BoxDecoration(color: Colors.white),
+                          child: Image.asset('resources/icons/google_logo.png', height: 25.0,)
+                        ),
+                        Text('Sign in with Google',
+                          style: Theme.of(context).textTheme.bodyText1.copyWith(
+                              fontFamily: DefaultTextStyle.of(context).style.fontFamily,
+                              fontWeight: FontWeight.w500,
+                              fontSize: Theme.of(context).textTheme.bodyText1.fontSize + 2,
+                              color: darkMode? Colors.black : Colors.white
+                          ),
+                        )
                       ],
                     ),
                     onPressed: () => userState.signInWithGoogle(),
                   ),
+                  Container(height: 20.0,),
                   FutureBuilder(
                     future: userState.isAppleSignInAvailable(),
                     builder: (context, snapshot) {
@@ -47,16 +65,13 @@ class LoginPage extends StatelessWidget {
                       return Visibility(
                         visible: snapshot.hasData && snapshot.data == true,
                         replacement: Container(),
-                        child: OutlineButton(
+                        child: SignInWithAppleButton(
+                          style: darkMode
+                            ? SignInWithAppleButtonStyle.white
+                            : SignInWithAppleButtonStyle.black,
+                          borderRadius: BorderRadius.all(Radius.circular(3.0)),
+                          height: 40,
                           key: ObjectKey('apple_sign_in'),
-                          child: Row(
-                            mainAxisSize: MainAxisSize.min,
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              Image.asset('resources/icons/apple_logo.png', height: 30.0,),
-                              Text('Sign in with Apple')
-                            ],
-                          ),
                           onPressed: () => userState.signInWithApple(),
                         ),
                       );
